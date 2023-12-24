c42380d6f452b204b95a5002d8e20c31b19668c6
Rasen Nikov <rasen800@gmail.com>
2023-12-24 16:46:51 +0600

    Добавил миграции

--- NewsPortal/news/models.py
+++ NewsPortal/news/models.py
@@ -1,3 +1,43 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=200, unique=True)
+
+
+class Post(models.Model):
+    news = 'NE'
+    articles = 'AR'
+
+    TYPES = [
+        (news, 'Новости'),
+        (articles, 'Статьи')
+    ]
+
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    choice_types = models.CharField(max_length=2, choices=TYPES, default=news)
+    time_in = models.DateTimeField(default=timezone.now) # time_in = models.DateTimeField(default=timezone.now)
+    categories = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=250, default='')
+    content = models.TextField(blank=False)
+    rate_new = models.IntegerField(default=0)
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment_text = models.TextField(blank=False)
+    comment_time_in = models.DateTimeField(timezone.now)
+    comment_rate = models.IntegerField(default=0)
