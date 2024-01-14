dabd39a55cc306b1fd5d5ef3ba214c596f3395b7
Rasen Nikov <rasen800@gmail.com>
2024-01-14 13:13:01 +0600

    Обновление

--- NewsPortal/work/settings.py
+++ NewsPortal/work/settings.py
@@ -38,8 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_filters',
+    'crispy_forms',
+    "crispy_bootstrap5",
     'news.apps.NewsConfig',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
